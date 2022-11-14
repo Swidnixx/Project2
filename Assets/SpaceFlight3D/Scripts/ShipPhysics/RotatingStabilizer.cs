75ccc7309f47454ce6760fcb004c2d4b0c371628
Swidnixx <swidnixx@gmail.com>
2022-11-14 21:16:35 +0100

    Tuned Flight Stabilizer

---
+++ Assets/SpaceFlight3D/Scripts/ShipPhysics/RotatingStabilizer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingStabilizer : SpaceShipStabilizer
+{
+    protected override void AddForce()
+    {
+        float counterGravityOntoEngineUp = 2 - (Vector3.Dot(-Physics.gravity, transform.up) / Physics.gravity.magnitude);
+        Debug.Log("Counter gravity: " + counterGravityOntoEngineUp);
+        force = power * (transform.up * counterGravityOntoEngineUp * Physics.gravity.magnitude * Mass);
+        force += -rb.velocity * velocityCounterForce;
+
+        rb.AddForce(force);
+    }
+}
--- Assets/SpaceFlight3D/Scripts/ShipPhysics/SpaceShipStabilizer.cs
+++ Assets/SpaceFlight3D/Scripts/ShipPhysics/SpaceShipStabilizer.cs
@@ -1,30 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SpaceShipStabilizer : MonoBehaviour
 {
     public float Power { get { return power; } set { value = Mathf.Clamp(value, 0, 1); power = value; } }
+    protected float Mass { get { return rb.mass + childrenBodies.Sum(x => x.mass); } }
 
     [Range(0, 1)]
-    [SerializeField] float power = 1;
+    [SerializeField]protected float power = 1;
     [Range(0,1)]
-    [SerializeField] float velocityCounterForce = 1;
+    [SerializeField]protected float velocityCounterForce = 1;
 
-    Rigidbody rb;
-    Vector3 force;
-    float verticalInput;
+    protected Rigidbody rb;
+    protected Rigidbody[] childrenBodies;
+    protected Vector3 force;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Rigidbody[] tmp = GetComponentsInChildren<Rigidbody>();
+        childrenBodies = new Rigidbody[tmp.Length - 1];
+        for(int i=0; i< childrenBodies.Length; i++)
+        {
+            childrenBodies[i] = tmp[i + 1];
+        }
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, transform.up);
+        Debug.DrawRay(transform.position, force / Mass, Color.cyan);
+        Debug.Log(Mass);
     }
 
     private void FixedUpdate()
     {
-        force = power * -Physics.gravity + -rb.velocity * velocityCounterForce;
-        //force *= power;
+        AddForce();
+    }
 
-        rb.AddForce( force, ForceMode.Acceleration );
+    protected virtual void AddForce()
+    {
+        force = power * -Physics.gravity * Mass + -rb.velocity * velocityCounterForce;
+
+        rb.AddForce(force);
     }
 }
